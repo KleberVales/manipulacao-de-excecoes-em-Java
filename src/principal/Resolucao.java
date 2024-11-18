6a989a9a41d945f83ca508ad03b8c71fb4e4c983
Kleber Vales <ksvales@outlook.pt>
2024-11-18 09:57:17 -0300

    Resolucao do exercicio

--- src/principal/Resolucao.java
+++ src/principal/Resolucao.java
@@ -1,5 +1,22 @@
 package principal;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Resolucao {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		try {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			int c = x / y;
+			System.out.println(c);
+		} catch (InputMismatchException q) {
+			System.out.println("java.util.InputMismatchException");
+		} catch (Exception q) {
+			System.out.println(q);
+		}
+	}
 }
